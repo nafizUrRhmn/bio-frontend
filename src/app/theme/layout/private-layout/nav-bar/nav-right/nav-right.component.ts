8fc8f85d49e3ca0fedf3b27b1738b400a205603b
ahasan34039 <tanvirehsan60@gmail.com>
2024-03-03 16:04:05 +0600

    all the navbars now use a same component

--- src/app/theme/layout/private-layout/nav-bar/nav-right/nav-right.component.ts
+++ src/app/theme/layout/private-layout/nav-bar/nav-right/nav-right.component.ts
@@ -1,7 +1,5 @@
 // Angular import
 import { Component } from '@angular/core';
-import {AuthenticationService} from "../../../../../_services";
-import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-nav-right',
@@ -9,22 +7,5 @@ import {TranslateService} from "@ngx-translate/core";
   styleUrls: ['./nav-right.component.scss']
 })
 export class NavRightComponent {
-  username: any;
-  constructor(private authService: AuthenticationService,
-              private translate: TranslateService) {
-      let jwt ="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZ2VudGJhbmsiLCJyb2xlcyI6IlJPTEVfU19BRE1JTixST0xFX1VTRVIiLCJpYXQiOjE3MDY2OTM0NTAsImV4cCI6MTcwNjY5Mzc1MH0.K-Qs88-2Ke0xe5gjzlpE8pbt_Cb4yq65YIzA19ND1ww";
-      let encodedPayload = jwt.split('.')[1];
-      let decodedPayload = window.atob(encodedPayload);
-      let parsedPayload = JSON.parse(decodedPayload);
-      this.username = parsedPayload.sub;
-      console.log(this.username);
-  }
-
-  translateLanguageTo(lang: string) {
-    console.log(lang);
-    this.translate.use(lang);
-  }
-  onLogout(){
-    this.authService.logout();
-  }
+  
 }
