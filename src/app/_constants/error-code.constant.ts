f32d7e14cec73016f932862c5350734117f1fd41
nafizUrRhmn <nafizurrahman.cse@gmail.com>
2024-03-10 15:11:18 +0600

    fix: change integer to string

--- src/app/_constants/error-code.constant.ts
+++ src/app/_constants/error-code.constant.ts
@@ -1,13 +1,13 @@
 export class ErrorCodeConstant{
-  public static readonly ALREADY_LOGGED_IN = -1;
-  public static readonly STAY_ON_SAME_SCREEN = -2;
-  public static readonly PASSWORD_CHANGE_SCREEN_ERROR_CODE = -3;
-  public static readonly INVALID_SECURITY_TOKEN = -4;
-  public static readonly DEVICE_MISMATCH = -5;
-  public static readonly DATE_COMPARE_ERR = -6;
-  public static readonly GEN_CLASS_EXCEPTION = -9;
-  public static readonly GEN_ERROR_FROM_DTO = -10;
-  public static readonly GENERAL_ERROR_CODE = -9999;
-  public static readonly NULL_MSG_FROM_BACKEND = -9998;
-  public static readonly DO_REFLECT_ERR_CODE = -9997;
+  public static readonly ALREADY_LOGGED_IN = '-1';
+  public static readonly STAY_ON_SAME_SCREEN = '-2';
+  public static readonly PASSWORD_CHANGE_SCREEN_ERROR_CODE = '-3';
+  public static readonly INVALID_SECURITY_TOKEN = '-4';
+  public static readonly DEVICE_MISMATCH = '-5';
+  public static readonly DATE_COMPARE_ERR = '-6';
+  public static readonly GEN_CLASS_EXCEPTION = '-9';
+  public static readonly GEN_ERROR_FROM_DTO = '-10';
+  public static readonly GENERAL_ERROR_CODE = '-9999';
+  public static readonly NULL_MSG_FROM_BACKEND = '-9998';
+  public static readonly DO_REFLECT_ERR_CODE = '-9997';
 }
