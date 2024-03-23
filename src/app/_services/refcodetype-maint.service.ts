d307b4c184c2b907d0896f437b85eaca55a7087c
Nusrat Tarin <nusrat.mopa@gmail.com>
2024-03-23 22:51:50 +0600

    1.refcode type maintenance submit function implementation.

--- src/app/_services/refcodetype-maint.service.ts
+++ src/app/_services/refcodetype-maint.service.ts
@@ -2,13 +2,33 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from "../../environments/environment";
 
+class Refcodetype {
+  functionCode?:string;
+  refCodeType?:string;
+  refCodeTypeDesc?:string;
+  deleteFlg?:string;
+  lchgTime?:string;
+  dependentFlg?:string;
+  dependentRefCodeType?:string;
+  depRefCodeTypeDesc?:string;
+  refCodeLength?:string;
+  menuId?:string;
+}
 @Injectable({providedIn: 'root'})
-export class RefcodetypeMaintService {
+export class RefCodeTypeMaintService {
   baseUrl = `${environment.apiUrl}/v1`;
   constructor(
     private http: HttpClient) {
   }
-  getRefTypeList(funCode:string, refTypeOrDsc:string) {
-    return this.http.post<any>(`${this.baseUrl}/get-refType-list`, {funCode, refTypeOrDsc})
+  getRefTypeList(funCode:string, refCodeType:string) {
+    return this.http.post<any>(`${this.baseUrl}/get-refType-list`, {funCode, refCodeType})
+  }
+
+  getRefTypeDetail(funCode:string, refCodeType:string) {
+    return this.http.post<any>(`${this.baseUrl}/get-refType-detail`, {funCode, refCodeType})
+  }
+
+  submit(payLoad:Refcodetype){
+    return this.http.post<any>(`${this.baseUrl}/submit`, payLoad)
   }
 }
--- src/app/private/core-config/refcodetype-maint/refcodetype-maint.component.ts
+++ src/app/private/core-config/refcodetype-maint/refcodetype-maint.component.ts
@@ -1,34 +1,50 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MatDialog} from "@angular/material/dialog";
-import {RefcodetypeMaintService} from "../../../_services/refcodetype-maint.service";
 import {AlertService} from "../../../_services/alert-service";
 import {AgbListComponent} from "../../../shared/components/agb-list/agb-list.component";
+import {MatStepper} from "@angular/material/stepper";
+import {RefCodeTypeMaintService} from "../../../_services/refcodetype-maint.service";
 
 @Component({
   selector: 'app-refcodetype-inquiry',
   templateUrl: './refcodetype-maint.component.html',
   styleUrls: ['./refcodetype-maint.component.scss']
 })
-export class RefCodeTypeMaintComponent {
-  private selectedData: any;
-  isLinear = false;
+export class RefCodeTypeMaintComponent implements OnInit {
+
+  @ViewChild('stepper',{read:MatStepper}) stepper:MatStepper
 
   constructor(private fb: FormBuilder,
               private alertService: AlertService,
-              private refCodeService: RefcodetypeMaintService,
+              private refCodeService: RefCodeTypeMaintService,
               public dialog: MatDialog) {
   }
 
-  refCodeTypeForm: FormGroup = this.fb.group({
-    funcCode: ['', [Validators.required]],
-    refCodeType: ['', [Validators.required]],
-  });
+  refCodeTypeForm: FormGroup;
+  refDetailForm: FormGroup;
+  private selectedData: any;
+  isLinear = false;
   refCodeDesc: string;
+  depRefCodeDesc: string;
 
-  onSearch() {
-    let funCode = this.funcCode.value;
-    let refTypeOrDsc = this.refCodeType.value;
+  ngOnInit() {
+    this.refCodeTypeForm = this.fb.group({
+      funcCode: ['', [Validators.required]],
+      refCodeType: ['', [Validators.required]],
+    });
+    this.refCodeDesc = '';
+
+    this.refDetailForm = this.fb.group({
+        refDescription: ['', [Validators.required]],
+        refLength: ['', [Validators.required]],
+        dependentFlg: ['', [Validators.required]],
+        depRefType: [{value: '', disabled: true}, [Validators.required]],
+        deleteFlg: [, [Validators.required]]
+    });
+  }
+
+  onSearch(funCode:string, refTypeOrDsc:string) {
     this.refCodeService.getRefTypeList(funCode, refTypeOrDsc).subscribe({
       next: (response) => {
         this.openDialogue(response);
@@ -51,11 +67,40 @@ export class RefCodeTypeMaintComponent {
     });
   }
 
+  onNext() {
+    this.refCodeService.getRefTypeDetail(this.funcCode.value, this.refCodeType.value).subscribe({
+      next: (response) => {
+        if(response.formData){
+          this.setDetailFormFields(response.formData);
+          this.stepper.next();
+        }
+      },
+      error: err => {
+        this.alertService.errorAlert(err.error.message);
+      }
+    });
+  }
   private setFormFields(selectedData: any) {
     this.refCodeTypeForm.controls["refCodeType"].setValue(selectedData.refCodeType);
     this.refCodeDesc = selectedData.refCodeTypeDesc;
   }
+  setDetailFormFields(refCodeTypeDetail:any) {
+    console.log(refCodeTypeDetail);
+    this.refDetailForm.controls['refDescription'].setValue(refCodeTypeDetail.refCodeTypeDesc);
+    this.refDetailForm.controls['refLength'].setValue(refCodeTypeDetail.refCodeLen);
+    this.refDetailForm.controls['dependentFlg'].setValue(refCodeTypeDetail.depFlg);
+    if(refCodeTypeDetail.depFlg === 'Y'){
+      this.refDetailForm.controls['depRefType'].setValue(refCodeTypeDetail.depRefCodeType);
+      this.depRefCodeDesc =  refCodeTypeDetail.depRefCodeTypeDesc;
+      this.refDetailForm.controls['depRefType'].enable();
+    }
+    else{
+      this.refDetailForm.controls['depRefType'].setValue('');
+      this.refDetailForm.controls['depRefType'].disable();
+    }
+    this.refDetailForm.controls['deleteFlg'].setValue(refCodeTypeDetail.delFlg);
 
+  }
   get refCodeType() {
     return this.refCodeTypeForm.get('refCodeType');
   }
@@ -64,11 +109,54 @@ export class RefCodeTypeMaintComponent {
     return this.refCodeTypeForm.get('funcCode');
   }
 
+  get refDescription() {
+    return this.refDetailForm.get('refDescription');
+  }
+
+  get length() {
+    return this.refDetailForm.get('length');
+  }
+
+  get dependentFlg() {
+    return this.refDetailForm.get('dependentFlg');
+  }
+
+  get depRefType() {
+    return this.refDetailForm.get('depRefType');
+  }
+
+  get deleteFlg() {
+    return this.refDetailForm.get('deleteFlg');
+  }
+
   onFocusOutEvent($event: any) {
   }
 
-  onNext(){
 
+  onCancel() {
+    this.refDetailForm.reset();
   }
 
+  onSubmit() {
+    const payLoad = {
+      "functionCode": this.funcCode.value,
+      "refCodeType": this.refCodeType.value,
+      "refCodeTypeDesc": this.refDescription.value,
+      "deleteFlg": this.deleteFlg.value,
+      "lchgTime": null,
+      "dependentFlg": this.dependentFlg.value,
+      "dependentRefCodeType": this.depRefType.value,
+      "depRefCodeTypeDesc": this.depRefCodeDesc,
+      "refCodeLength": this.length.value,
+      "menuId": 'RCTMM'
+    };
+
+    this.refCodeService.submit(payLoad).subscribe({
+      next: (response) => {
+      },
+      error: err => {
+        this.alertService.errorAlert(err.error.message);
+      }
+    });
+  }
 }
