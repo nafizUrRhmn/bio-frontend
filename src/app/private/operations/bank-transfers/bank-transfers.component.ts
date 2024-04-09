8ffd6f089b9b7d550f73ca439a2ddcec984e2c38
ahasan34039 <tanvirehsan60@gmail.com>
2024-04-09 12:18:57 +0600

    updated cash operations and fund transfer

--- src/app/private/operations/bank-transfers/bank-transfers.component.ts
+++ src/app/private/operations/bank-transfers/bank-transfers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormFormatService } from 'src/app/_services/form-format.service';
 
 @Component({
   selector: 'app-bank-transfers',
@@ -9,17 +10,66 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class BankTransfersComponent implements OnInit {
   transactionForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  accountNumberVar: string;
+  transferTypeVar: string;
+  transferAmountVar: string;
+  purposeOfPaymentVar: string;
+  transactionRemarksVar: string;
+  transactionParticularVar: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private formFormat: FormFormatService
+    ) {}
 
   ngOnInit(): void {
     this.transactionForm = this.fb.group({
-      accNum: ['', [Validators.required]],
+      accNum: ['', [Validators.required, this.maxLengthValidator]],
       transferType: ['', [Validators.required]],
       transferAmount: ['', [Validators.required]],
       purposeOfPayment: ['', [Validators.required]],
       transactionRemarks: ['', [Validators.required]],
       transactionParticular: ['', [Validators.required]],      
     });
+  }
+
+  maxLengthValidator(control: FormControl) {
+    const value = control.value;
+    const maxLengths = [13, 16]; 
+    if (value && value.length && !maxLengths.includes(value.length)) {
+      return { maxLength: true };
+    }
+  
+    return null;
+  }
+
+  onInput(event: any) {
+    this.formFormat.onAmountInput(event);
+  }
+
+  onBlur(event: any) {
+    this.formFormat.onCurrency(event);
+  }
+
+  onProceed() {
+    this.accountNumberVar = this.transactionForm.get('accNum').value;
+    this.transferTypeVar = this.transactionForm.get('transferType').value;
+    this.transferAmountVar = this.transactionForm.get('transferAmount').value;
+    this.purposeOfPaymentVar = this.transactionForm.get('purposeOfPayment').value;
+    this.transactionRemarksVar = this.transactionForm.get('transactionRemarks').value;
+    this.transactionParticularVar = this.transactionForm.get('transactionParticular').value;
+
+    //CHECKS IF ALL THE REQUIRED FORM FIELDS ARE FILLED OR NOT
+    if (this.transactionForm.invalid) {
+      this.transactionForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.accountNumberVar, this.transferTypeVar, this.transferAmountVar, this.purposeOfPaymentVar,this.transactionRemarksVar,this.transactionParticularVar)
+  }
+
+  onReset() {
+    this.transactionForm.reset();
   }
 
 }
--- src/app/private/operations/loan-disbursement/loan-disbursement.component.ts
+++ src/app/private/operations/loan-disbursement/loan-disbursement.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loan-disbursement',
@@ -9,14 +9,43 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoanDisbursementComponent implements OnInit {
   loanDisb: FormGroup;
 
+  linkAccountNumber:string;
+
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.loanDisb = this.fb.group({
-      linkAccNo: ['', Validators.required]
+      linkAccNo: ['', [Validators.required,this.maxLengthValidator]]
     });
   }
   get linkAccNo() {
     return this.loanDisb.get('linkAccNo');
   }
+
+  maxLengthValidator(control: FormControl) {
+    const value = control.value;
+    const maxLengths = [13, 16]; 
+    if (value && value.length && !maxLengths.includes(value.length)) {
+      return { maxLength: true };
+    }
+  
+    return null;
+  }
+
+  onProceed() {
+    this.linkAccountNumber = this.loanDisb.get('linkAccNo').value;
+  
+    //CHECKS IF ALL THE REQUIRED FORM FIELDS ARE FILLED OR NOT
+    if (this.loanDisb.invalid) {
+      this.loanDisb.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.linkAccountNumber)
+  }
+
+  onReset() {
+    this.loanDisb.reset();
+  }
+
 }
