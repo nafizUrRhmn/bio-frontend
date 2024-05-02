6daa1a36883b6f7a8ef50b2c9e38135166cf42d7
muqtadir-61 <zidanmuqtadir@gmail.com>
2024-05-02 11:11:14 +0600

    Validation add complete (without Language grid required validation)

--- src/app/private/core-config/refcode-maint/refcode-maint.component.ts
+++ src/app/private/core-config/refcode-maint/refcode-maint.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
-import { MatDialog } from "@angular/material/dialog";
-import { AgbListComponent } from "../../../shared/components/agb-list/agb-list.component";
-import { NavigationService } from "../../../theme/layout/private-layout/navigation/nav-content/navigation.service";
-import { RefCodeMaintService } from "./refcode-maint.service";
-import { take } from "rxjs";
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { AgbListComponent } from '../../../shared/components/agb-list/agb-list.component';
+import { NavigationService } from '../../../theme/layout/private-layout/navigation/nav-content/navigation.service';
+import { RefCodeMaintService } from './refcode-maint.service';
+import { take } from 'rxjs';
 import { MatStepper } from '@angular/material/stepper';
-import { AlertService } from 'src/app/_services/alert-service';
+import { AlertService } from '../../../_services/alert-service';
+import { englishOnlyValidator, precisionValidator,englishOnlyValidatorForFormArray } from '../../../_custom-validator/custom-validators.component';
+import { CommonUtil } from '../../../_helpers/common.util';
 
 @Component({
   selector: 'app-refcode-maint',
@@ -14,7 +16,7 @@ import { AlertService } from 'src/app/_services/alert-service';
   styleUrls: ['./refcode-maint.component.scss']
 })
 export class RefCodeMaintComponent implements OnInit {
-
+   classInitializer = CommonUtil.classInitializer;
   isLinear = false;
   mopCodeDescList: any;
   refTypeDesc: string;
@@ -24,160 +26,195 @@ export class RefCodeMaintComponent implements OnInit {
   funcCodeOptions = [];
   isHiddenRefCodeSrchBtn: boolean;
   depFlg: string;
-  delFlg:string;
-  menuId:string;
-  lchgTime:string;
-  depRefCodeType:string;
+  delFlg: string;
+  menuId: string;
+  lchgTime: string;
+  depRefCodeType: string;
   depRefCodeDesc: any;
   isInquiry: boolean = false;
   isDelete: boolean = false;
+  isHiddenDefRefCode: Boolean;
 
-  @ViewChild('stepper', { read: MatStepper }) stepper: MatStepper
+  onNextPayload: any;
 
-  constructor(private fb: FormBuilder,
+  @ViewChild('stepper', { read: MatStepper }) stepper: MatStepper;
+
+  constructor(
+    private fb: FormBuilder,
     public dialog: MatDialog,
     public refCodeMaintService: RefCodeMaintService,
     private navService: NavigationService,
-    private alertService: AlertService,
-  ) {
-  }
+    private alertService: AlertService
+  ) // private utilsService: UtilsService,
+  {}
 
-  ngOnInit() {
-    //this.funcCodeOptions = this.navService.getPermittedOptions();
+  ngOnInit() {    
+    this.funcCodeOptions=this.navService.getPermittedOptions();
     this.refCodeForm = this.fb.group({
       funcCode: ['', [Validators.required]],
-      refCodeType: ['', [Validators.required]],
-      refCode: ['', Validators.required],
+      refCodeType: ['', [Validators.required, englishOnlyValidator()]],
+      refCode: ['', [Validators.required, englishOnlyValidator()]],
       depFlg: [''],
-      delFlg:[''],
-      lchgTime:[''],
-      menuId:[''],
-
+      delFlg: [''],
+      lchgTime: [''],
+      menuId: ['']    
     });
     this.refCodeDetailForm = this.fb.group({
       shortListFlg: ['', [Validators.required]],
-      depRefCode: [''],
+      depRefCode: ['', [Validators.required, englishOnlyValidator()]],      
       language: this.fb.array([])
     });
     this.isHiddenRefCodeSrchBtn = false;
+    this.isHiddenDefRefCode = false;
   }
+
 
   onSrchRefType(funcCode: string, refTypeOrDsc: string) {
     const payLoad = {
-      "functionCode": funcCode,
-      "refCodeType": refTypeOrDsc
+      functionCode: funcCode,
+      refCodeType: refTypeOrDsc
     };
     const dialogRef = this.dialog.open(AgbListComponent, {
       width: '50%',
       data: {
         title: 'Reference Type Data',
         serviceName: 'getRefTypeList',
-        srchPayLoad: payLoad,
+        srchPayLoad: payLoad
       },
       disableClose: true
     });
 
-    dialogRef.afterClosed().pipe(take(1)).subscribe(selectedRow => {
-      this.refCodeType.setValue(selectedRow?.refCodeType);
-      this.refTypeDesc = selectedRow?.refCodeTypeDesc;
-    });
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((selectedRow) => {
+        console.log(selectedRow);
+        this.refCodeType.setValue(selectedRow?.refCodeType);
+        this.refTypeDesc = selectedRow?.refCodeTypeDesc;
+
+        if (selectedRow !== null && selectedRow !== undefined) {
+          this.refCodeForm.get('refCodeType').disable();
+        }
+      });
   }
 
   onSrchRefCode(funcCode: string, refTypeOrDesc: string, refCodeOrDesc: string) {
     const payLoad = {
-      "funcCode": funcCode,
-      "refCodeType": refTypeOrDesc,
-      "refCode": refCodeOrDesc
+      funcCode: funcCode,
+      refCodeType: refTypeOrDesc,
+      refCode: refCodeOrDesc
     };
     const dialogRef = this.dialog.open(AgbListComponent, {
       width: '50%',
       data: {
         title: 'Reference Code Data',
         serviceName: 'getRefCodeList',
-        srchPayLoad: payLoad,
+        srchPayLoad: payLoad
       },
       disableClose: true
     });
-    dialogRef.afterClosed().pipe(take(1)).subscribe(selectedRow => {
-      this.refCode.setValue(selectedRow?.refCode);
-      this.refCodeDesc = selectedRow?.refCodeDesc;
-    });
-  }
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((selectedRow) => {
+        this.refCode.setValue(selectedRow?.refCode);
+        this.refCodeDesc = selectedRow?.refCodeDesc;
 
+        if (selectedRow !== null && selectedRow !== undefined) {
+          this.refCodeForm.get('refCode').enable();
+        }
+      });
+  }
 
   onSrchdepRefCode(funcCode: string, refTypeOrDesc: string, refCodeOrDesc: string) {
     const payLoad = {
-      "funcCode": funcCode,
-      "refCodeType": refTypeOrDesc,
-      "refCode": refCodeOrDesc
+      funcCode: funcCode,
+      refCodeType: refTypeOrDesc,
+      refCode: refCodeOrDesc
     };
     const dialogRef = this.dialog.open(AgbListComponent, {
       width: '50%',
       data: {
         title: 'Reference Code Data',
         serviceName: 'getRefCodeList',
-        srchPayLoad: payLoad,
+        srchPayLoad: payLoad
       },
       disableClose: true
     });
-    dialogRef.afterClosed().pipe(take(1)).subscribe(selectedRow => {
-      console.log(selectedRow);
-      this.depRefCode.setValue(selectedRow?.refCode);
-      this.depRefCodeDesc = selectedRow?.refCodeDesc;
-    });
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((selectedRow) => {
+        console.log(selectedRow);
+        this.depRefCode.setValue(selectedRow?.refCode);
+        this.depRefCodeDesc = selectedRow?.refCodeDesc;
+      });
   }
 
   onNext() {
-    if (this.refCodeForm.invalid) {
-      return;
-    }
-
-    const payLoad = {
-      "funcCode": this.funcCode.value,
-      "refCodeType": this.refCodeType.value,
-      "refCode": this.refCode.value,
-
+    this.onNextPayload = {
+      funcCode: this.funcCode.value,
+      refCodeType: this.refCodeType.value,
+      refCode: this.refCode.value
     };
-    this.refCodeMaintService.getRefCodeDetail(payLoad)
+    this.refCodeMaintService
+      .getRefCodeDetail(this.onNextPayload)
       .pipe(take(1))
       .subscribe((response) => {
         if (response.genDataBlock.formData) {
-          this.depRefCodeType = response.genDataBlock.formData.depRefCodeType
+          this.depRefCodeType = response.genDataBlock.formData.depRefCodeType;
           this.depRefCodeDesc = response.genDataBlock.formData.depRefCodeDesc;
           this.depFlg = response.genDataBlock.formData.depFlg;
           this.lchgTime = response.genDataBlock.formData.lchgTime;
           this.refCodeForm.patchValue({ depFlg: this.depFlg });
-          this.refCodeForm.patchValue({delFlg:'N'});
-          this.refCodeForm.patchValue({menuId:'RCMM'});
-          this.refCodeForm.patchValue({lchgTime:this.lchgTime});
+          this.refCodeForm.patchValue({ delFlg: 'N' });
+          this.refCodeForm.patchValue({ menuId: 'RCMM' });
+          this.refCodeForm.patchValue({ lchgTime: this.lchgTime });
           this.refCodeDetailForm.patchValue(response.genDataBlock.formData);
           this.mopCodeDescList = this.mrhBlockToGrid(response.mrhBlock.mrhBlocks[0]);
 
-          console.log(this.refCodeDetailForm);
+          console.log(response);
 
           const mrh = this.gridToMrhBlock(this.mopCodeDescList);
-           console.log(response);
+          console.log(response);
+          if (this.funcCode.value == 'A') {
+            this.refCodeDetailForm.patchValue({ depRefCode: '' });
+          }
 
           for (const mop of this.mopCodeDescList) {
             let featureFrom = new FormGroup({
-              refCodeDesc: new FormControl(''),
+              refCodeDesc: new FormControl('',englishOnlyValidatorForFormArray(mop.refCodeLang)),
               refCodeLang: new FormControl(''),
               refCodeLangDesc: new FormControl(''),
               delFlg: new FormControl('N'),
-              lchgTime:new FormControl(''),
+              lchgTime: new FormControl('')
             });
             (this.refCodeDetailForm.get('language') as FormArray).push(featureFrom);
           }
           this.refCodeDetailForm.get('language').patchValue(<any[]>this.mopCodeDescList);
         }
       });
-  };
+  }
 
+  menuDesc(i){
+    console.log(i)
+    return (this.refCodeDetailForm?.get('language') as FormArray)?.controls[i]?.get('refCodeDesc');
+  }
+
+  onReset() {
+    this.refCodeForm.reset();
+    this.refCodeForm.get('refCode').enable();
+    this.refCodeForm.get('refCodeType').enable();
+    this.isHiddenRefCodeSrchBtn=false;
+    this.refCodeDesc = '';
+    this.refTypeDesc = '';
+    this.onNextPayload = '';
+  }
 
   mrhBlockToGrid(mrhBlock: any) {
     let list = [];
     for (let i = 0; i < mrhBlock.dataBlock.length; i++) {
-      let obj = {}
+      let obj = {};
       for (let j = 0; j < mrhBlock.headerInfo.length; j++) {
         let key = mrhBlock.headerInfo[j];
         let value = mrhBlock.dataBlock[i][j];
@@ -188,14 +225,13 @@ export class RefCodeMaintComponent implements OnInit {
     return list;
   }
 
-
   gridToMrhBlock(list: any[]) {
     let mrhBlock = {
       headerInfo: [],
       dataBlock: []
     };
 
-       if (list.length > 0) {
+    if (list.length > 0) {
       mrhBlock.headerInfo = Object.keys(list[0]);
     }
 
@@ -210,35 +246,43 @@ export class RefCodeMaintComponent implements OnInit {
     return mrhBlock;
   }
 
-
   onChangeFuncCode(event: any) {
-    this.refCodeForm.patchValue({ refCodeType: '', refCode: '' });
+    // this.refCodeForm.patchValue({ refCodeType: '', refCode: '' });
+    this.refCodeForm.get('refCode').enable();
+    this.refCodeForm.get('refCodeType').enable();
     this.refTypeDesc = '';
     this.refCodeDesc = '';
     switch (this.funcCode.value) {
-      case 'I': {
-        this.isInquiry = (event.target.value === 'I');
-        this.isHiddenRefCodeSrchBtn = false;
-      }
+      case 'I':
+        {
+          this.isInquiry = event.target.value === 'I';
+          this.isHiddenRefCodeSrchBtn = false;
+          this.isHiddenDefRefCode = true;
+        }
         break;
-      case 'C': {
-        this.isDelete = true;
-      }
+      case 'C':
+        {
+          this.isDelete = true;
+        }
         break;
       case 'D':
-      case 'V': {
-      }
+      case 'V':
+        {
+        }
         break;
-      case 'A': {
-        this.isInquiry = false;
-        this.isHiddenRefCodeSrchBtn = true;
-      }
+      case 'A':
+        {
+          this.isInquiry = false;
+          this.isHiddenRefCodeSrchBtn = true;
+          this.isHiddenDefRefCode = false;
+        }
         break;
       case 'U':
       case 'M':
-      case 'X': {
-        this.isHiddenRefCodeSrchBtn = false;
-      }
+      case 'X':
+        {
+          this.isHiddenRefCodeSrchBtn = false;
+        }
         break;
       default:
         break;
@@ -252,29 +296,40 @@ export class RefCodeMaintComponent implements OnInit {
   onSubmit() {
     const payload = {
       ...this.refCodeForm.value,
-      ...this.refCodeDetailForm.value
+      ...this.refCodeDetailForm.value,
+      'refCodeType': this.onNextPayload.refCodeType
     };
+    console.log(this.onNextPayload);
     console.log(payload);
 
-    this.refCodeMaintService.submit(payload).pipe(take(1)).subscribe({
-      next: (v) => {
-        this.alertService.successAlert(v.responseMessage)
-          .then(() => this.refCodeForm.reset(),
-          ).then(() => this.refTypeDesc = '').then(() => this.stepper.reset());
-      },
-      error: (err) => {
-        this.alertService.errorAlert(err.error.message);
-      }
-    });
+    this.refCodeMaintService
+      .submit(payload)
+      .pipe(take(1))
+      .subscribe({
+        next: (v) => {
+          this.alertService
+            .successAlert(v.responseMessage)
+            .then(() => this.refCodeForm.reset())
+            .then(() => (this.refTypeDesc = ''))
+            .then(() => this.stepper.reset())
+            .then(() => this.refCodeForm.get('refCodeType').enable())
+            .then(() => this.isHiddenRefCodeSrchBtn=false)
+            ;
+        },
+        error: (err) => {
+          this.alertService.errorAlert(err.error.message);
+        }
+      });
   }
 
-
-   clearRefCodeFormFields() {
+  clearRefCodeFormFields() {
     this.refCodeForm.reset();
-    this.refCodeDesc='';
-    this.refTypeDesc='';
+    this.refCodeForm.get('refCode').enable();
+    this.refCodeForm.get('refCodeType').enable();
+    this.isHiddenRefCodeSrchBtn=false;
+    this.refCodeDesc = '';
+    this.refTypeDesc = '';
   }
-
 
   get funcCode() {
     return this.refCodeForm.get('funcCode');
@@ -300,7 +355,5 @@ export class RefCodeMaintComponent implements OnInit {
     return this.refCodeForm.get('langCode');
   }
 
-  onFocusOutEvent($event: any) {
-  }
-
+  onFocusOutEvent($event: any) {}
 }
