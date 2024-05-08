0dca29f608b64e5911351dd4942b3ffdefb896f1
muqtadir-61 <zidanmuqtadir@gmail.com>
2024-05-08 11:12:26 +0600

    Additional-info design complete (without validation and grid data payload)

--- src/app/private/operations/onboarding/additional-info/additional-info.component.ts
+++ src/app/private/operations/onboarding/additional-info/additional-info.component.ts
@@ -1,29 +1,116 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, NgModule, Output, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {OnboardingConstant} from "../onboarding.constant";
 import {MatExpansionModule} from "@angular/material/expansion";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import { CommonModule } from '@angular/common';
+
+import {NrxGridComponent} from "../../../../shared/components/nrx-grid/nrx-grid.component";
+import {ColDef} from "ag-grid-community";
+import {AgGridModule} from 'ag-grid-angular';
+import { NrxGridModule } from 'src/app/shared/components/nrx-grid/nrx-grid.module';
+
+import {
+  englishOnlyValidator,
+  englishOnlyValidatorForFormArray,
+  numbersOnlyValidator
+} from "../../../../_custom-validator/custom-validators.component";
+import {CommonUtil} from "../../../../_helpers/common.util";
+
+
 
 @Component({
   selector: 'app-additional-info',
   templateUrl: './additional-info.component.html',
   standalone: true,
-  imports: [MatExpansionModule, FormsModule, ReactiveFormsModule],
+  imports: [MatExpansionModule, FormsModule, ReactiveFormsModule,MatDatepickerModule,
+    MatNativeDateModule,MatInputModule,AgGridModule,NrxGridModule,CommonModule],
   styleUrls: ['./additional-info.component.scss']
 })
 
+
 export class AdditionalInfoComponent {
+  classInitializer = CommonUtil.classInitializer;
+
+  @ViewChild(NrxGridComponent) nrxGrid: NrxGridComponent;
+
+  incomeSegmentList: any[] = [];
+  incomeSummaryTbl: ColDef[] = [
+    { headerName: 'Income Source', field: 'incomeSource' },
+    { headerName: 'Amount', field: 'amount', 
+    editable: true, cellEditor: 'agTextCellEditor' }
+  ];
+
+
+
+  initializeData() {    
+    for (let i = 0; i < 6; i++) {
+       if (i === 5) {
+        this.incomeSegmentList.push({ incomeSource: 'Total Amount', totalamount: '' });
+      } else {
+        switch (i) {
+          case 1:
+            this.incomeSegmentList.push({ incomeSource: 'Agriculture', agriAmount: '' });
+            break;
+          case 2:
+            this.incomeSegmentList.push({ incomeSource: 'Business', amount: '' });
+            break;
+          case 3:
+            this.incomeSegmentList.push({ incomeSource: 'Rent', amount: '' });
+            break;
+          case 4:
+            this.incomeSegmentList.push({ incomeSource: 'Salary', amount: '' });
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
   panelOpenState = false;
   detailsForm: FormGroup;
   @Output() submitEvent =  new EventEmitter<any>();
   @Output() previousEvent = new EventEmitter<any>();
+
+
+
   constructor(private fb: FormBuilder) {
+    
+    this.initializeData();
     this.detailsForm = this.fb.group({
-      firstName: ['', Validators.required],
-      middleName: ['', Validators.required],
-      lastName: ['', Validators.required]
+
+      //Employment Details
+
+      organizationName:['',[Validators.required,englishOnlyValidator()]],
+      position:[''],
+      otherBusiness:[''],
+      monthlyIncomeRange:['',[]],
+      sourceOfIncome:[''],
+      natureOfbusiness:[''],
+      detailsOFprof:[''],
+
+      //Introducer Information
+
+      accountNo:[''],
+      cifId:[''],
+      accountTitle:[''],
+      accountName:[''],
+      relationWithIntro:[''],
+      yearofknowIntro:[''],
+      dob:[''],
+
+      // firstName: ['', Validators.required],
+      // middleName: ['', Validators.required],
+      // lastName: ['', Validators.required]
     });
   }
 
+  get organizationName() {
+    return this.detailsForm.get('organizationName');
+  }
 
   onSubmit(): void {
     console.log(this.detailsForm.value);
@@ -38,3 +125,5 @@ export class AdditionalInfoComponent {
 
 
 }
+
+
