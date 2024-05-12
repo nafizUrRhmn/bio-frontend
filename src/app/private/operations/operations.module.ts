a2d05454b9f121ea2185d1fa296686b916b1aa0e
nafizUrRhmn <nafizurrahman.cse@gmail.com>
2024-05-12 10:29:52 +0600

    feat: remove duplicate import

--- src/app/private/operations/operations.module.ts
+++ src/app/private/operations/operations.module.ts
@@ -40,14 +40,12 @@ import {CorporateComponent} from './corporate/corporate.component';
 import {TranslateModule} from "@ngx-translate/core";
 import {ReportComponent} from './report/report.component';
 import {OnboardingComponent} from "./onboarding/onboarding.component";
-import {MatStepperModule} from "@angular/material/stepper";
 import {DetailsComponent} from "./onboarding/details/details.component";
 import {ContactsComponent} from "./onboarding/contacts/contacts.component";
 import {AdditionalInfoComponent} from "./onboarding/additional-info/additional-info.component";
 import {DeclarationsComponent} from "./onboarding/declarations/declarations.component";
 import {DocumentsComponent} from "./onboarding/documents/documents.component";
 import {AccountPreferencesComponent} from "./onboarding/account-preferences/account-preferences.component";
-import { TakePictureDialogComponent } from './onboarding/take-picture-dialog/take-picture-dialog.component';
 import {MatButtonModule} from "@angular/material/button";
 import {MatStepperModule} from "@angular/material/stepper";
 
@@ -116,7 +114,7 @@ const routes: Routes = [
         DeclarationsComponent,
         DocumentsComponent,
         AccountPreferencesComponent,
-      MatButtonModule
+        MatButtonModule
     ],
 })
 export class OperationsModule {
